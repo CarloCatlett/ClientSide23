c18d4028ad791df5de2b33861f426838a065c270
CarloCatlett <122460339+CarloCatlett@users.noreply.github.com>
2023-04-14 23:22:40 -0500

    Update project09-01b.js

--- chapter9/project09-01/project09-01b.js
+++ chapter9/project09-01/project09-01b.js
@@ -1,1 +1,28 @@
 
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Project 09-01
+
+      Project to read field values from a query string
+      Author: Carlo Catlett
+      Date: 4/13/2023
+
+      Filename: project09-01b.js
+*/
+
+const query = location.search.slice(1);
+const modify = query.replace(/\+/g, ' ');
+const decode = decodeURIComponent(query);
+const cardFields = query.split('&');
+
+
+for (const item of cardFields) {
+      const nameValue = item.split('=');
+      const name = nameValue[0];
+      const value = nameValue[1];
+      const element = document.getElementById(name);
+      if (element) {
+        element.textContent = value;
+      }
+    }
